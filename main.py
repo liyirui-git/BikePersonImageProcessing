5e227a0faaf735625af28e151bbfc34d66b02cf3
iry-lee <28668523+iry-lee@users.noreply.github.com>
2021-02-21 07:55:05 +0000

    在服务器上的版本

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ import segmentation
 BLACK = [0, 0, 0]
 
 # data_processing.get_query_from_test_images()
+print("query seg begin")
+segmentation.seg_person_from_mask("BikePersonDatasetNew/query-seg", BLACK)
+print("test seg begin")
+segmentation.seg_person_from_mask("BikePersonDatasetNew/test-seg", BLACK)
+print("end!")
 
-segmentation.seg_person_from_mask("BikePersonDatasetNew/query-seg-test", BLACK)
