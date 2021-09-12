ad626f5c023badc35de5a35c2ce16a9069f46c16
iry-lee <28668523+iry-lee@users.noreply.github.com>
2021-09-12 12:07:51 +0000

    增加预测视角分类的结果输出，停止对dml_ped的支持

--- keypoint_to_angle.py
+++ keypoint_to_angle.py
@@ -5,27 +5,31 @@ Description: transform human pose like OpenPose format to angle size
 FilePath: /liyirui/PycharmProjects/BikePersonImageProcessing/keypoint_to_angle.py
 '''
 # public lib
-from multiprocessing import set_forkserver_preload
 import os
 import glob
 import json
 import re
 import shutil
+from sys import float_repr_style
 from types import LambdaType
 import numpy as np
 import math
 import imagesize  # https://github.com/shibukawa/imagesize_py
 import cv2
+import random
 
 # my lib
 import utils
 from mylog import MyLog
 
 # some global varible
-DEBUG = True
-TEST = True
-MODE = 'bikeperson'  # 'bikeperson', 'dml_ped'
-
+DEBUG = False
+TEST_GET_MAIN_CHARACTER = False
+MODE = 'bikeperson_3_view'  # 'bikeperson_3_view', 'bikeperson_4_view'
+'''
+bikeperson_3_view: back, front, side
+bikeperson_4_view: back, front, left, right
+'''
 
 def select_main_character(pose_data):
     '''
@@ -97,11 +101,14 @@ class Keypoint2Angle:
                        "left_knee":13, "left_ankle":14, "right_eye":15, "left_eye":16, 
                        "right_ear":17, "left_ear": 18}
     
-    def __init__(self, dataset_path, dataset_name, output_path, format='openpose'):
+    def __init__(self, dataset_path, dataset_name, label_path, output_folder, format='openpose'):
         self.dataset_path = dataset_path
         self.dataset_name = dataset_name
+        self.label_path = label_path
         self.format = format
-        self.output_path = output_path
+        self.output_folder = output_folder
+        self.output_path = os.path.join(dataset_path, output_folder)
+        self.query_angle_label_dir = {}
         if os.path.exists(self.output_path):
             shutil.rmtree(self.output_path)
         if os.path.exists(self.output_path + ".zip"):
@@ -109,6 +116,13 @@ class Keypoint2Angle:
         if not os.path.exists(self.output_path):
             os.mkdir(self.output_path)
         
+        # initialize the query angle view label
+        angle_view_folder_list = ["back_view", "front_view", "left_view", "right_view"]
+        for folder_name in angle_view_folder_list:
+            folder_path = os.path.join(self.label_path, folder_name)
+            for image_name in os.listdir(folder_path):
+                self.query_angle_label_dir[image_name] = folder_name
+
     def openpose_dataloader(self):
         '''
         detail:
@@ -180,6 +194,9 @@ class Keypoint2Angle:
             if DEBUG:
                 break
         return image_name_list, pose_data_dir
+
+    def get_standard_label_for_debug_mode(self, image_name):
+        return self.query_angle_label_dir[image_name + ".png"]
 
     def vector_included_angle(self, v1, v2):
         '''
@@ -333,6 +350,12 @@ class Keypoint2Angle:
             args['confi_threshold']=0.7
         if 'calculator' not in args:
             args['calculator'] = "shoulder_calculator"
+        if 'mode' not in args:
+            args['mode'] = MODE
+        if 'zip' not in args:
+            args['zip'] = False
+        if 'output_label' not in args:
+            args['output_label'] = False
         
         return args
 
@@ -347,14 +370,21 @@ class Keypoint2Angle:
         hw_ratio_threshold = args['hw_ratio_threshold']
         confi_threshold = args['confi_threshold']
         calculator = args['calculator']
+        mode = args['mode']
+        if_zip = args['zip']
+        if_output_label = args['output_label']
 
         ml.write("---------------------- args ---------------------", color='g')
         args_str = utils.dic_to_string(args)
         ml.write(args_str)
         
+        # do keypoint to angle
         image_name_list, pose_data_dir = self.openpose_dataloader()
-        ct_0, ct_1, ct_n = 0, 0, 0
+        ct_0 = 0
         ct_back, ct_front, ct_side, ct_left, ct_right = 0, 0, 0, 0, 0
+        ct_back_correct, ct_front_correct, ct_side_correct, ct_left_correct, ct_right_correct = 0, 0, 0, 0, 0
+        image_name_angle_view_dir = {}
+
         for image_name in image_name_list:
             '''
             format of 'pose_data' can be found in openpose_dataloader()
@@ -366,97 +396,151 @@ class Keypoint2Angle:
                                                         os.path.join(self.output_path, "side_view"),
                                                         os.path.join(self.output_path, "left_view"),
                                                         os.path.join(self.output_path, "right_view")])
-
-            if TEST:
+            '''
+            if TEST_GET_MAIN_CHARACTER:
                 if len(pose_data['pose']) > 1:
                     pose_data = select_main_character(pose_data=pose_data)
                     image = cv2.imread(pose_data["rendered_image_path"])
                     image = paint_keypoints_on_image(pose_data['pose'], image)
                     cv2.imwrite(os.path.join("test", pose_data['image_name']+".png"), image)
                 continue
-
-            if len(pose_data['pose']) == 0:
+            '''
+            if len(pose_data['pose']) == 0: 
                 ct_0 = ct_0 + 1
-            # 暂时仅处理图片中检测到一个行人的
-            elif len(pose_data['pose']) == 1:
-                # 1. 先用长宽比判断, 长宽比小于阈值的图片直接作为侧视图
-                #                  长宽比大于阈值的图片在后续的判断中再决定是那种视图
-                image_size = pose_data['image_size']
-                if image_size['height'] / image_size['width'] < hw_ratio_threshold:
-                    theta, confi = 180, 1
+            # 如果图片中检测得到多个人，可能是多人骑一辆车，也可能是存在遮挡，则先选取主要的那个人
+            if len(pose_data['pose']) > 1: 
+                pose_data = select_main_character(pose_data=pose_data)
+
+            # 1. 先用长宽比判断, 长宽比小于阈值的图片直接作为侧视图
+            #                  长宽比大于阈值的图片在后续的判断中再决定是那种视图
+            image_size = pose_data['image_size']
+            if image_size['height'] / image_size['width'] < hw_ratio_threshold:
+                theta, confi = 180, 1
+
+            # 2. 对长宽比粗分完以后，没有被归为侧视图的图像，使用shoulder_hip_calculator来判断属于那种视图
+            # 先处理检测到人的图像
+            elif len(pose_data['pose']) > 0:
+                if calculator == "shoulder_hip_calculator":
+                    theta, confi = self.shoulder_hip_calculator(pose_data['pose'][0])
                 else:
-                    if calculator == "shoulder_hip_calculator":
-                        theta, confi = self.shoulder_hip_calculator(pose_data['pose'][0])
-                    else:
-                        theta, confi = self.shoulder_calculator(pose_data['pose'][0])
-                    # 2. 根据置信度分类（前提是长宽比这个阈值存在），如果置信度如果低于阈值，直接根据判断角度是否是大于
-                    if hw_ratio_threshold > 0 and confi < confi_threshold:
-                        if theta > 180: 
-                            theta, confi = 270, 0
-                        else: 
-                            theta, confi = 90, 0
+                    theta, confi = self.shoulder_calculator(pose_data['pose'][0])
+                # 3. 根据置信度分类（前提是长宽比这个阈值存在），
+                #    如果置信度如果低于阈值，直接根据判断角度是否是大于180度，划分视角
+                if hw_ratio_threshold > 0 and confi < confi_threshold:
+                    if theta > 180: 
+                        theta, confi = 270, 0
+                    else: 
+                        theta, confi = 90, 0
+            else:
+                # 对应，没有检测到人的图像，则随机分配前后视角
+                if random.randint(1,2) == 1:
+                    theta, confi = 270, 0
+                else: 
+                    theta, confi = 90, 0
                 
-                # 对图片按照视角分类
-                dst_image_name = "t" + str(theta) + "_c" + str(confi) + "_" + image_name + ".png"
-                if theta >= 270-angle_threshold and theta <= 270+angle_threshold:
-                    dst_path = os.path.join(front_dir, dst_image_name)
-                    ct_front = ct_front + 1
-                elif theta >= 90-angle_threshold and theta <= 90+angle_threshold:
-                    dst_path = os.path.join(back_dir, dst_image_name)
-                    ct_back = ct_back + 1
-                else:
-                    # 如果是bikeperson数据集，则判断人的左右朝向
-                    if MODE == 'bikeperson':
-                        if self.towards_of_side_view(pose_data['pose'][0]) == "left_view":
-                            dst_path = os.path.join(left_dir, dst_image_name)
-                            ct_left = ct_left + 1
+            # 3. 对图片按照之前计算得到的视角进行分类
+            dst_image_name = "t" + str(theta) + "_c" + str(confi) + "_" + image_name + ".png"
+            # front_view
+            if theta >= 270-angle_threshold and theta <= 270+angle_threshold:
+                dst_path = os.path.join(front_dir, dst_image_name)
+                ct_front = ct_front + 1
+                if DEBUG and self.get_standard_label_for_debug_mode(image_name) == "front_view":
+                    ct_front_correct = ct_front_correct + 1
+                image_name_angle_view_dir[image_name] = "front_view"
+            # back_view
+            elif theta >= 90-angle_threshold and theta <= 90+angle_threshold:
+                dst_path = os.path.join(back_dir, dst_image_name)
+                ct_back = ct_back + 1
+                if DEBUG and self.get_standard_label_for_debug_mode(image_name) == "back_view":
+                    ct_back_correct = ct_back_correct + 1
+                image_name_angle_view_dir[image_name] = "back_view"
+            # side_view
+            else:
+                # 如果是bikeperson数据集，则判断人的左右朝向
+                if mode == 'bikeperson_4_view':
+                    angle_view = ""
+                    # 如果不存在人，则随机分配左右视角
+                    if len(pose_data['pose']) == 0:
+                        if random.randint(1,2) == 1:
+                            angle_view = "left_view"
                         else:
-                            dst_path = os.path.join(right_dir, dst_image_name)
-                            ct_right = ct_right + 1
+                            angle_view = "right_view"
                     else:
-                        dst_path = os.path.join(side_dir, dst_image_name)
-                        ct_side = ct_side + 1
-                # display pose rendered image rather than original image.
-                shutil.copyfile(pose_data['rendered_image_path'], dst_path)
-                ct_1 = ct_1 + 1
-                
-            else: ct_n = ct_n + 1
+                        angle_view = self.towards_of_side_view(pose_data['pose'][0])
+
+                    if angle_view == "left_view":
+                        dst_path = os.path.join(left_dir, dst_image_name)
+                        ct_left = ct_left + 1
+                        if DEBUG and self.get_standard_label_for_debug_mode(image_name) == "left_view":
+                            ct_left_correct = ct_left_correct + 1
+                        image_name_angle_view_dir[image_name] = "left_view"
+                    else:
+                        dst_path = os.path.join(right_dir, dst_image_name)
+                        ct_right = ct_right + 1
+                        if DEBUG and self.get_standard_label_for_debug_mode(image_name) == "right_view":
+                            ct_right_correct = ct_right_correct + 1
+                        image_name_angle_view_dir[image_name] = "right_view"
+                else:
+                    dst_path = os.path.join(side_dir, dst_image_name)
+                    ct_side = ct_side + 1
+                    if DEBUG:
+                        view = self.get_standard_label_for_debug_mode(image_name)
+                        if view == "left_view" or view == "right_view":
+                            ct_side_correct = ct_side_correct + 1
+                    image_name_angle_view_dir[image_name] = "side_view"
+            # display pose rendered image rather than original image.
+            shutil.copyfile(pose_data['rendered_image_path'], dst_path)
         
         ml.write("---------------------- statistic ---------------------", color='g')
         ml.write("image which not detected person number: \t" + str(ct_0))
-        ml.write("image which detected 1 person number: \t\t" + str(ct_1))
-        ml.write("image which detected >=2 person number: \t" + str(ct_n))
         ml.write("---------------------- result ---------------------", color='g')
-        ml.write("back view: " + str(ct_back) + "\nfront view: " + str(ct_front))
-        if MODE == "bikeperson":
-            ml.write("left view: " + str(ct_left) + "\nright view: " + str(ct_right))
-        elif MODE == "dml_ped":
+        ml.write("back view: " + str(ct_back))
+        if DEBUG:
+            ml.write("correct rate: " + str(ct_back_correct) + "/" + str(ct_back) + "=" + str(ct_back_correct/ct_back) + "\n")
+        ml.write("front view: " + str(ct_front))
+        if DEBUG:
+            ml.write("correct rate: " + str(ct_front_correct) + "/" + str(ct_front) + "=" + str(ct_front_correct/ct_front) +"\n")
+        if mode == "'bikeperson_4_view'":
+            ml.write("left view: " + str(ct_left))
+            if DEBUG:
+                ml.write("correct rate: " + str(ct_left_correct) + "/" + str(ct_left) + "=" + str(ct_left_correct/ct_left) +"\n")
+            ml.write("right view: " + str(ct_right))
+            if DEBUG:
+                ml.write("correct rate: " + str(ct_right_correct) + "/" + str(ct_right) + "=" + str(ct_right_correct/ct_right) +"\n")
+        elif mode == "bikeperson":
             ml.write("side view: " + str(ct_side))
+            if DEBUG:
+                ml.write("correct rate: " + str(ct_side_correct) + "/" + str(ct_side) + "=" + str(ct_side_correct/ct_side) +"\n")
+       
+
+        if if_zip:
+            utils.color_print("---------------------- zip ---------------------", color="g")
+            os.system("cd " + self.dataset_path + "&& zip -qr " + self.output_folder + ".zip " + self.output_folder + "/")
+            ml.write("ZIP finished.\nResult in " + os.path.join(self.dataset_path, self.output_folder+".zip"))
+
+        if if_output_label:
+            utils.color_print("---------------------- output label ---------------------", color="g")
+            json_format_dic = json.dumps(image_name_angle_view_dir)
+            output_file = open(os.path.join(self.output_path, "angle_label.json"), "w")
+            output_file.write(json_format_dic)
+            output_file.close()
+            ml.write("Output label finished.\nResult in " + os.path.join(self.output_path, "angle_label.json"))
+        
         ml.close()
 
 if __name__ == "__main__":
     # init different varible for different mode
-    if MODE == 'bikeperson':
+    if MODE == 'bikeperson_3_view' or MODE == "'bikeperson_4_view'":
         dataset_path = "/home/liyirui/PycharmProjects/dataset"
         dataset_name = "BikePerson-700-origin"
-        output_folder = "keypoint2angle_bikeperson"
-        output_path = os.path.join(dataset_path, output_folder)
-        args = {}
-    elif MODE == 'dml_ped':
-        dataset_path = "/home/liyirui/PycharmProjects/dataset"
-        dataset_name = "dml_ped12_market"
-        output_folder = "keypoint2angle_dml_ped12"
-        output_path = os.path.join(dataset_path, output_folder)
-        args = {'hw_ratio_threshold': 0, 
-                'confi_threshold': 0.5,
-                'calculator': 'shoulder_hip_calculator'}
+        output_folder = "keypoint2angle_" + MODE
+        label_path = os.path.join(dataset_path, "standard_angle_bikeperson")
+        # Can refer to method: make_args()
+        args = {'mode': MODE, 'output_label': True}
     else:
         utils.color_print("[Error]: Uncorrect MODE value.")
         exit()
     
-    kp2a = Keypoint2Angle(dataset_path, dataset_name, output_path)
+    kp2a = Keypoint2Angle(dataset_path, dataset_name, label_path, output_folder)
     kp2a.run(args)
-    utils.color_print("---------------------- zip ---------------------", color="g")
-    os.system("cd " + dataset_path + "&& zip -qr " + output_folder + ".zip " + output_folder + "/")
-    print("ZIP finished.\nResult in " + os.path.join(dataset_path, output_folder+".zip"))
     
